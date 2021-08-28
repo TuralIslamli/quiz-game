b2b7e263cb4bb34209a3df2bdf3983e3d8b39c23
aidaidaid <aidashirinovaaa@gmail.com>
2021-08-29 02:29:06 +0400

    contact page

---
+++ js/contact.js
@@ -0,0 +1,45 @@
+function stopDefAction(event) {
+    event.preventDefault();
+}
+
+document.querySelector('.send').addEventListener(
+    'click', stopDefAction, false
+); 
+
+function changeContent() {
+
+    if (document.querySelector('h1').textContent == 'Ваше письмо отправлено') {
+      document.querySelectorAll('.remove').forEach(el => el.style.display = 'inline-block');
+      document.querySelectorAll('.remove').forEach(el => el.value = "");
+      document.querySelector('h1').textContent = 'Написать нам';
+      document.querySelector('.send').value = 'Отправить';
+    } else {
+      inputs = document.querySelectorAll('.formEntry');
+      validation = false;
+      for (let i = 0; i<inputs.length; i++) {
+        validation = inputs[i].checkValidity();
+        if (validation == false) break;
+      }
+      if (validation) {
+        console.log('valid'); 
+        document.querySelectorAll('.remove').forEach(el => el.style.display = 'none');
+        document.querySelector('h1').textContent = 'Ваше письмо отправлено';
+        document.querySelector('.send').value = 'Отправить новое';
+        sendEmail();
+      } else {
+        alert('Пожалуйста, заполните все поля'); 
+      }
+    }
+}
+
+function sendEmail() {
+	Email.send({
+	Host: "smtp.gmail.com",
+	Username : "aid.shirinova@gmail.com",
+	Password : "forproject",
+	To : 'aid.shirinova@gmail.com',
+	From : document.querySelector('.email').value,
+	Subject : "Сообщение от пользователя",
+	Body : document.querySelector('.message').value
+	})
+}
