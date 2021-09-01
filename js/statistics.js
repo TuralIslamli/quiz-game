52491f4c65c5f7bbaf4a85f32678b0930925c0f5
Emin Zeynalov <emin@wizardsdev.com>
2021-09-01 20:56:23 +0400

    fix merge

--- js/statistics.js
+++ js/statistics.js
@@ -1,52 +1,14 @@
-let scienceButton = document.getElementById('science-button');
-let scienceStatistics = document.getElementById('science-statistics');
-scienceButton.addEventListener("dblclick",function(){
-    scienceStatistics.style.display="block";
-});
-scienceButton.addEventListener("click",function(){
-    scienceStatistics.style.display="none";
-});
-let artButton = document.getElementById('art-button');
-let artStatistics = document.getElementById('art-statistics');
-artButton.addEventListener("dblclick",function(){
-    artStatistics.style.display="block";
-});
-artButton.addEventListener("click",function(){
-    artStatistics.style.display="none";
-});
-let sportButton = document.getElementById('sport-button');
+const accordion = document.getElementsByClassName("categories-button");
 
-let sportStatistics = document.getElementById('sport-statistics');
-sportButton.addEventListener("dblclick",function(){
-    sportStatistics.style.display="block";
-});
-sportButton.addEventListener("click",function(){
-    sportStatistics.style.display="none";
-});
-let musicButton = document.getElementById('music-button');
+for (let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function() {
+    this.classList.toggle("_active");
 
-let musicStatistics = document.getElementById('music-statistics');
-musicButton.addEventListener("dblclick",function(){
-    musicStatistics.style.display="block";
-});
-musicButton.addEventListener("click",function(){
-    musicStatistics.style.display="none";
-});
-let medicineButton = document.getElementById('medicine-button');
-
-let medicineStatistics = document.getElementById('medicine-statistics');
-medicineButton.addEventListener("dblclick",function(){
-    medicineStatistics.style.display="block";
-});
-medicineButton.addEventListener("click",function(){
-    medicineStatistics.style.display="none";
-});
-let geoButton = document.getElementById('geo-button');
-
-let geoStatistics = document.getElementById('geo-statistics');
-geoButton.addEventListener("dblclick",function(){
-    geoStatistics.style.display="block";
-});
-geoButton.addEventListener("click",function(){
-    geoStatistics.style.display="none";
-});
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
