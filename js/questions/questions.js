aff49db55b744f722a13fa1825c31099a007fabc
aidaidaid <aidashirinovaaa@gmail.com>
2021-09-02 19:17:04 +0400

    statistics answers

--- js/questions/questions.js
+++ js/questions/questions.js
@@ -3,13 +3,25 @@ localStorage.removeItem('arrayOfQuestions');
 let script = document.createElement('script');
 script.src = 'js/questions/questionsOf' + categoryName + '.js';
 document.body.appendChild(script);
+
+let questionNum = 1;
+let playerScore = 0;
+let wrongAnswersScore = 0;
+let index = 0;
+let arr = [];
 ////////
 // if(localStorage.getItem("statistics"+ categoryName)!= null) {
 //     let neededCategoryStatistics = localStorage.setItem("statistics"+ categoryName, JSON.stringify(categoryName+'Obj'));
 //     } else {
 //     let neededCategoryStatistics = localStorage.getItem("statistics"+ categoryName);
 //     }
-///////
+
+
+// function pushStatistics() {
+//     // localStorage.setItem('games', );
+// }
+/////
+
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
     return myTimer = setInterval(function () {
@@ -24,6 +36,7 @@ function startTimer(duration, display) {
             document.querySelector(".question_text").innerHTML = 'ВРЕМЯ ВЫШЛО <br><br> ИГРА ОКОНЧЕНА';
             document.querySelectorAll(".hide").forEach((el)=>el.style.display = 'none');
             document.querySelector('.timer').style.display="none";
+            endGame();
         }
     }, 1000);
 }
@@ -37,11 +50,8 @@ const setNewQuestions = () => {
 
     if (isArrayInStorage) {
         document.querySelector("h2").innerText = 'Выбранная категория: ' + localStorage.getItem('categoryİnRus');
-        let questionNum = 1;
-        let playerScore = 0;
-        let wrongAnswersScore = 0;
-        let index = 0;
-        let arr = [];
+        localStorage.removeItem('rightAnswers');
+        localStorage.removeItem('wrongAnswers');
         nextQuestion(0);
 
         let fiveMinutes = 60*3;
@@ -87,8 +97,8 @@ const setNewQuestions = () => {
 
             document.querySelector('.right').innerText = playerScore;
             document.querySelector('.wrong').innerText = wrongAnswersScore;
-            localStorage.setItem('rightAnswers', playerScore);
-            localStorage.setItem('wrongAnswers', wrongAnswersScore);
+            // localStorage.setItem('rightAnswers', playerScore);
+            // localStorage.setItem('wrongAnswers', wrongAnswersScore);
 
             answer = document.getElementsByClassName('answer');
             for (let i = 0; i < answer.length; i++) {
@@ -99,13 +109,14 @@ const setNewQuestions = () => {
         isFalseAnswerBtn.addEventListener('click', () => checkForAnswer(isFalseAnswerBtn.innerText))
 
         function handleNextQuestion() {
-            if (index <= 19) {
+            if (index <= 1) {
                 nextQuestion(index);
             } else {
                 clearInterval(myTimer);
                 document.querySelector(".question_text").innerHTML = 'ИГРА ОКОНЧЕНА';
                 document.querySelectorAll(".hide").forEach((el)=>el.style.display = 'none');
                 document.querySelector('.timer').style.display="none";
+                endGame();
             }
             document.getElementsByClassName('question_content')[0].style.backgroundColor = "#DC5866";
             answer = document.getElementsByClassName('answer');
@@ -119,6 +130,50 @@ const setNewQuestions = () => {
             setNewQuestions();
         }, 5000);
     }
+}
+
+function incrementCounter() {
+  let getCounterFromStore = localStorage.getItem('cntOf' + categoryName);
+  function countUp() {
+    getCounterFromStore ++;
+    console.log(getCounterFromStore);
+    return getCounterFromStore;
+  }
+  return countUp;
+};
+
+function incrementRightAnswers() {
+  let getRightAnsFromStore = localStorage.getItem('rightAns' + categoryName);
+  function countUp() {
+    getRightAnsFromStore = parseInt(getRightAnsFromStore) + playerScore;
+    console.log(getRightAnsFromStore);
+    return getRightAnsFromStore;
+  }
+  return countUp;
+};
+
+let countOfArt = incrementCounter();
+let countOfGeography = incrementCounter();
+let countOfMedicine = incrementCounter();
+let countOfMusic = incrementCounter();
+let countOfScience = incrementCounter();
+let countOfSport = incrementCounter();
+//
+let rightAnswersArt = incrementRightAnswers()
+let rightAnswersGeography = incrementRightAnswers()
+let rightAnswersMedicine = incrementRightAnswers()
+let rightAnswersMusic = incrementRightAnswers()
+let rightAnswersScience = incrementRightAnswers()
+let rightAnswersSport = incrementRightAnswers()
+
+
+function setToLocalStore() {
+    localStorage.setItem('cntOf'+categoryName, eval(`countOf${categoryName}()`))
+    localStorage.setItem('rightAns'+categoryName, eval(`rightAnswers${categoryName}()`))
+    // localStorage.setItem('wrongAns'+categoryName, eval(`wrongAnswers${categoryName}()`))
+}
+function endGame(){
+    setToLocalStore()
 }
 
 setNewQuestions();
--- js/statistics.js
+++ js/statistics.js
@@ -1,8 +1,40 @@
-const accordion = document.getElementsByClassName("categories-button");
+let accordion = document.getElementsByClassName("categories-button");
+let categoryName = localStorage.getItem('category')
+console.log(categoryName);
+let countOfArt = document.getElementById(`count_of_Art`)
+let countOfGeography = document.getElementById(`count_of_Geography`)
+let countOfMedicine = document.getElementById(`count_of_Medicine`)
+let countOfMusic = document.getElementById(`count_of_Music`)
+let countOfScience = document.getElementById(`count_of_Science`)
+let countOfSport = document.getElementById(`count_of_Sport`)
 
+countOfArt.innerText = localStorage.getItem('cntOfArt');
+countOfGeography.innerText = localStorage.getItem('cntOfGeography');
+countOfMedicine.innerText = localStorage.getItem('cntOfMedicine');
+countOfMusic.innerText = localStorage.getItem('cntOfMusic');
+countOfScience.innerText = localStorage.getItem('cntOfScience');
+countOfSport.innerText = localStorage.getItem('cntOfSport');
+
+document.getElementById(`Art_Right_Ans`).innerText = localStorage.getItem('rightAnsArt')
+document.getElementById(`Geography_Right_Ans`).innerText = localStorage.getItem('rightAnsGeography')
+document.getElementById(`Medicine_Right_Ans`).innerText = localStorage.getItem('rightAnsMedicine')
+document.getElementById(`Music_Right_Ans`).innerText =  localStorage.getItem('rightAnsMusic')
+document.getElementById(`Science_Right_Ans`).innerText = localStorage.getItem('rightAnsScience')
+document.getElementById(`Sport_Right_Ans`).innerText = localStorage.getItem('rightAnsSport')
+
+// document.getElementById(`Art_Wrong_Ans`).innerText = localStorage.getItem('wrongAnsArt')
+// document.getElementById(`Geography_Wrong_Ans`).innerText = localStorage.getItem('wrongAnsGeography')
+// document.getElementById(`Medicine_Wrong_Ans`).innerText = localStorage.getItem('wrongAnsMedicine')
+// document.getElementById(`Music_Wrong_Ans`).innerText =  localStorage.getItem('wrongAnsMusic')
+// document.getElementById(`Science_Wrong_Ans`).innerText = localStorage.getItem('wrongAnsScience')
+// document.getElementById(`Sport_Wrong_Ans`).innerText = localStorage.getItem('wrongAnsSport')
+/////////////////
+
+let clickedCategory;
 for (let i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener("click", function() {
     this.classList.toggle("_active");
+    clickedCategory = this.classList[1];
 
     let panel = this.nextElementSibling;
     if (panel.style.display === "block") {
@@ -13,21 +45,28 @@ for (let i = 0; i < accordion.length; i++) {
   });
 }
 
-/////////////////////////
+////////////////
+statisticObj = "statistics"+ clickedCategory+'Obj';
+// games = localStorage.getItem('games');
+rightAnswers = localStorage.getItem('rightAnswers');
+wrongAnswers = localStorage.getItem('wrongAnswers');
 
-// function SetStatistics(games, rightAnswers, wrongAnswers) {
+// let statisticObj = {
+//   games: null,
+//   rightAnswers: null,
+//   wrongAnswers: null,
+//   rating: null,
+// }
+
+// function setStatistics(games, rightAnswers, wrongAnswers) {
 //   this.games = games;
 //   this.rightAnswers = rightAnswers;
 //   this.wrongAnswers = wrongAnswers;
 //   this.rating = (this.rightAnswers*100)/(this.games*20); 
-
 // }
 
+// setStatistics.call(statisticObj, )
+
 // let scienceScore = setStatistics()
-
-
-// document.getElementsByClassName('science').onclick = function() {
-//    let category = localStorage.getItem('category');
-// }​;​
 
 
